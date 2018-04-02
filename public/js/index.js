7e68253ac010a1a92ee173f26cfb8b5468018d40
Nyssa Keller <nyssa1@gmail.com>
2018-04-02 13:50:12 -0600

    Get delete to update with out needing refresh Get patch to work but it updates packed_status to null instead of true

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,7 @@ const renderItems = items => {
           <button class='delete-btn'>Delete</button>
         </section>
         <form class='checkbox-form'>
-          <input id='checkbox'
+          <input class='checkbox'
                  type='checkbox' />
           <p>Packed</p>
         </form>
@@ -33,24 +33,31 @@ const postItems = async(e) => {
   await renderItems([newItem]);
 }
 
-const deleteItem = async e => {
-  const id = e.target.parentElement.id
+const deleteItem = async(e, id) => {
   if (e.target.classList.contains('delete-btn')) {
     await fetch(`/api/v1/items_to_pack/${id}`, {
       method: 'DELETE'
     }) 
   }
+  await e.target.parentNode.parentNode.remove();
 }
 
-const checkItem = (e) => {
-  console.log(e.target)
+const checkItem = async(e, id) => {
+  e.target.classList.toggle('clicked');
+  if (e.target.classList.contains('clicked')) {
+    const patch = await fetch(`/api/v1/items_to_pack/${id}`, {
+      metod: 'PATCH'
+    })
+  console.log(await patch.json())
+  }
 }
 
 $('.card-container').on('click', (e) => {
+  const id = e.target.parentElement.id
   if (e.target.classList.contains('delete-btn')) {
-    deleteItem(e);
+    deleteItem(e, id);
   } else if (e.target.classList.contains('checkbox')) {
-    checkItem(e);
+    checkItem(e, id);
   }
 });
 
