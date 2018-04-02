5b00d51b1aaaff4284fc4dd513887bb150c854b0
Nyssa Keller <nyssa1@gmail.com>
2018-04-02 11:08:16 -0600

    Add function to get all items from the database and have each item render in the card container

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+const fetchItems = async() => {
+  const response = await fetch('/api/v1/items_to_pack');
+  const items = await response.json();
+  renderItems(items);
+}
+
+const renderItems = items => {
+  items.forEach(item => {
+    $('.card-container').prepend(`
+      <h1>${item.item_name}</h1>
+      <button>Delete</button>
+      <input type='checkbox' />
+    `)
+  })
+}
+
+$(document).ready(fetchItems())
+$('button').on('click', e => {
+  e.preventDefault();
+  const newItem = $('input').val();
+})
