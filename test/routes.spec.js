5c9fe4cc0c0da0a8c3961309a93bb92d549906f4
Nyssa Keller <nyssa1@gmail.com>
2018-04-02 10:16:11 -0600

    Write test for get items_to_pack

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -9,7 +9,7 @@ const database = require('knex')(configuration);
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  it('should return the homepage with text', () => {
+  it('should return the homepage', () => {
     return chai.request(server)
     .get('/')
     .then(response => {
@@ -30,6 +30,32 @@ describe('Client Routes', () => {
     .catch(err => {
       throw err;
     });
+  });
+
+});
+
+describe('API Routes', () => {
+
+  describe('GET /api/v1/students', () => {
+
+    it('should return all of the items to be packed', () => {
+      return chai.request(server)
+      .get('/api/v1/items_to_pack')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('item_name');
+        response.body[0].item_name.should.equal('Oxygen Tank');
+        response.body[0].should.have.property('packed_status');
+        response.body[0].packed_status.should.equal(false);
+      })
+      .catch(err => {
+        throw err;
+      });
+    });
+
   });
 
 });
