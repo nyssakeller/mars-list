ddc35472eb9ae06a4dec4ff02504b7a97fb04dc2
Nyssa Keller <nyssa1@gmail.com>
2018-04-02 10:03:53 -0600

    Get testing environment set up

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,16 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/mars_list_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds:  {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
   }
-
 };
