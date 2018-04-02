fa08a0545cfa4551e3a63d1c422255f256766735
Nyssa Keller <nyssa1@gmail.com>
2018-04-02 11:17:51 -0600

    Add post new projects fuction Items entered in the input are added to the database and render on the page on button click

--- public/js/index.js
+++ public/js/index.js
@@ -16,8 +16,19 @@ const renderItems = items => {
   })
 }
 
+const postItems = async(e) => {
+  const itemInput = $('input').val();
+  const postItem = await fetch('/api/v1/items_to_pack', {
+    method: 'POST',
+    body: JSON.stringify({ item_name: itemInput}), 
+    headers: new Headers({ 'Content-Type': 'application/json' })
+  })
+  const newItem = await postItem.json();
+  await renderItems([newItem]);
+}
+
 $(document).ready(fetchItems())
 $('button').on('click', e => {
   e.preventDefault();
-  const newItem = $('input').val();
+  postItems(e);
 })
